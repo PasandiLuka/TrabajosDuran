3770064f482bfe3546b8cd1c629a9d36f6fe6c64
PasandiLuka <lukapasandi@gmail.com>
2026-03-10 15:37:21 -0300

    Implementación logica constructor de Aldea

--- Vikingos/Biblitoteca/Entidades/Aldea.cs
+++ Vikingos/Biblitoteca/Entidades/Aldea.cs
@@ -2,6 +2,18 @@ namespace Biblitoteca.Entidades;
 
 public class Aldea
 {
-    public Iglesia iglesia;
-    public Amurallada amurallada;
+    public Iglesia? iglesia;
+    public Amurallada? amurallada;
+    
+    /// <summary>
+    /// Es solicitado definir si la aldea es amurallada o si posee una iglesia, mas no puede poseer ambas
+    /// </summary>
+    /// <param name="iglesia"></param>
+    /// <param name="amurallada"></param>
+    public Aldea(Iglesia? iglesia = null, Amurallada? amurallada = null)
+    {
+        if ((iglesia is null && amurallada is null) || (iglesia is not null && amurallada is not null)) throw new InvalidOperationException("La aldea debe ser amurallada o poseer una iglesia, más no debe poseer las dos");
+        this.iglesia = iglesia;
+        this.amurallada = amurallada;
+    }
 }
