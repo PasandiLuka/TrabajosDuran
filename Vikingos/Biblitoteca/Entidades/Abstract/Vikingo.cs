4baa4bf5d188b9f80574d7f76d61b0aeae60d8dd
PasandiLuka <lukapasandi@gmail.com>
2026-03-10 15:05:19 -0300

    Ojo?

--- Vikingos/Biblitoteca/Entidades/Abstract/Vikingo.cs
+++ Vikingos/Biblitoteca/Entidades/Abstract/Vikingo.cs
@@ -7,5 +7,10 @@ public abstract class Vikingo : Productividad
     public bool productivo;
     public Casta casta;
 
+    public void SubirCasta()
+    {
+        if (casta == Casta.Thrall) throw new InvalidOperationException("Ha alcanzado la casta máxima aventurero :D");
+        
+    }
     public void ChequearProductividad() => throw new NotImplementedException();
 }
