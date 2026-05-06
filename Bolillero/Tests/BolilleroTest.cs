14edaac1da43966cf5a2b077c3e6f54674fcfc18
PasandiLuka <lukapasandi@gmail.com>
2026-05-06 18:10:35 -0300

    Se pico

--- Bolillero/Tests/BolilleroTest.cs
+++ Bolillero/Tests/BolilleroTest.cs
@@ -1,5 +1,6 @@
 ﻿using Biblioteca.Entidades;
 using Biblioteca.Interfaces;
+using System.Diagnostics;
 
 namespace Tests;
 
@@ -9,13 +10,13 @@ public class AleatorioFijo : IAzar
 }
 public class BolilleroTest
 {
-    static AleatorioFijo _azar = new(); 
+    static AleatorioFijo _azar = new();
     Bolillero bolillero = new Bolillero(10, _azar);
 
     [Fact]
     public void CuandoJuegoParaGanar_DebeDevolverTrue()
     {
-        List<int> jugada = new(){0,1,2,3,4};
+        List<int> jugada = new() { 0, 1, 2, 3, 4 };
 
         Assert.True(bolillero.JugarGana(jugada));
     }
@@ -23,7 +24,7 @@ public class BolilleroTest
     [Fact]
     public void CuandoJuegoParaPerder_DebeDevolverTrue()
     {
-        List<int> jugada = new(){0,1,2,3,4};
+        List<int> jugada = new() { 0, 1, 2, 3, 4 };
 
         Assert.True(bolillero.JugarGana(jugada));
     }
@@ -35,10 +36,10 @@ public class BolilleroTest
     [InlineData(5)]
     public void CuandoJuegoNCantidadDeVeces_DebeDevolverTrue(int cantVeces)
     {
-        List<int> jugada = new(){0,1,2,3,4};
+        List<int> jugada = new() { 0, 1, 2, 3, 4 };
 
         Assert.Equal(cantVeces, bolillero.GanarNVeces(jugada, cantVeces));
-    } 
+    }
 
     [Theory]
     [InlineData(1)]
@@ -46,22 +47,54 @@ public class BolilleroTest
     [InlineData(3)]
     [InlineData(5)]
     public void CuandoJuegoNCantidadDeVecesYLaJugadaNoCoincide_DebeDevolverFalse(int cantVeces)
-    {        
-        List<int> jugada = new(){1,2,3,4,5};
+    {
+        List<int> jugada = new() { 1, 2, 3, 4, 5 };
 
         Assert.NotEqual(cantVeces, bolillero.GanarNVeces(jugada, cantVeces));
     }
 
+    // [Theory]
+    // [InlineData(10, 1)]
+    // [InlineData(40, 2)]
+    // [InlineData(23, 3)]
+    // [InlineData(55, 5)]
+    // [InlineData(int.MaxValue, 6)]
+    // public void CuandoJuegoNCantidadDeVecesEnNCantidadDeHilos_DebeRetornarLaCantidadDeJugadasAcertadas(int cantVeces, int cantHilos)
+    // {
+    //     List<int> jugada = new() { 1, 2, 3, 4, 5 };
+
+    //     Simulacion.SimularConHilos(bolillero, jugada, cantVeces, cantHilos);
+    // }
+
     [Theory]
-    [InlineData(10, 1)]
-    [InlineData(40, 2)]
-    [InlineData(23, 3)]
-    [InlineData(55, 5)]
-    [InlineData(999999, 200)]
-    public void CuandoJuegoNCantidadDeVecesEnNCantidadDeHilos_DebeRetornarLaCantidadDeJugadasAcertadas(int cantVeces, int cantHilos)
+    [InlineData(int.MaxValue, 6)]
+    public void CuandoJuegoNCantidadDeVecesEnNCantidadDeHilosContraCuandoJuegoSinHilos_LaSimulacionConHilosDebeSerMenorALaDeSinHilos(int cantVeces, int cantHilos)
     {
-        List<int> jugada = new(){1,2,3,4,5};
+        List<int> jugada = new() { 1, 2, 3, 4, 5 };
+        var tiempoConHilos = new TimeSpan();
+        var tiempoSinHilos = new TimeSpan();
+
+        Stopwatch cronometro = new Stopwatch();
+        cronometro.Start();
 
         Simulacion.SimularConHilos(bolillero, jugada, cantVeces, cantHilos);
+
+        cronometro.Stop();
+
+        tiempoConHilos = cronometro.Elapsed;
+
+        cronometro.Restart();
+
+        cronometro.Start();
+
+        Simulacion.SimularSinHilos(bolillero, jugada, cantVeces);
+
+        cronometro.Stop();
+
+        tiempoSinHilos = cronometro.Elapsed;
+
+        cronometro.Restart();
+
+        Assert.True(tiempoConHilos < tiempoSinHilos);
     }
 }
