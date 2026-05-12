5ee80e5b99737677a9905b9d4d76391c7581f6c1
Darkops56 <166878493+Darkops56@users.noreply.github.com>
2026-05-12 16:41:00 -0300

    Cambios para el async

--- Bolillero/Biblioteca/Entidades/Simulacion.cs
+++ Bolillero/Biblioteca/Entidades/Simulacion.cs
@@ -27,4 +27,26 @@ public class Simulacion
 
         return tareas.Sum(t => t.Result);
     }
+    public async static long SimularConHilosAsync(Bolillero bolillero, List<int> jugada, int cantVecesJugar, int cantHilos = 1)
+    {
+        var resto = cantVecesJugar % cantHilos;
+        var cantVecPorProceso = (cantVecesJugar - cantVecesJugar % cantHilos) / cantHilos;
+
+        Task<long>[] tareas = new Task<long>[cantHilos];
+
+        for(int i = 0; i < cantHilos; i++)
+        {
+            tareas[i] = Task.Run(() 
+                => bolillero.Clone().GanarNVeces(
+                    jugada, 
+                    cantVecPorProceso + 
+                        ((resto != 0 && i < resto) ? 1 : 0)
+                    )
+            );
+        }
+        
+        await Task.WhenAll(tareas);
+
+        return tareas.Sum(t => t.Result);
+    }
 }
